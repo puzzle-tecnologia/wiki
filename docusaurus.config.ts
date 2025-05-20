d0d33575a005fae3b465c27be111b40ead301036
Tiago Matana <tiago.matana@rarolabs.com.br>
2025-05-20 15:05:05 -0300

    testing action

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://docs.puzzle-tecnologia.com.br',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
